2d1693acc3e60212ea556ea6a92d78a00024acef
yozz <yozz.online@gmail.com>
2015-11-17 15:22:18 -0500

    Internationalization

--- startup-cpt-pricing-table.php
+++ startup-cpt-pricing-table.php
@@ -34,24 +34,24 @@ add_action( 'init', 'startup_reloaded_pricing_updater' );
 //CPT
 function startup_reloaded_pricing() {
 	$labels = array(
-		'name'                => _x( 'Price table items', 'Post Type General Name', 'text_domain' ),
-		'singular_name'       => _x( 'Price table item', 'Post Type Singular Name', 'text_domain' ),
-		'menu_name'           => __( 'Pricing table', 'text_domain' ),
-		'name_admin_bar'      => __( 'Pricing table', 'text_domain' ),
-		'parent_item_colon'   => __( 'Parent Item:', 'text_domain' ),
-		'all_items'           => __( 'All Items', 'text_domain' ),
-		'add_new_item'        => __( 'Add New Item', 'text_domain' ),
-		'add_new'             => __( 'Add New', 'text_domain' ),
-		'new_item'            => __( 'New Item', 'text_domain' ),
-		'edit_item'           => __( 'Edit Item', 'text_domain' ),
-		'update_item'         => __( 'Update Item', 'text_domain' ),
-		'view_item'           => __( 'View Item', 'text_domain' ),
-		'search_items'        => __( 'Search Item', 'text_domain' ),
-		'not_found'           => __( 'Not found', 'text_domain' ),
-		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' )
+		'name'                => _x( 'Price table items', 'Post Type General Name', 'startup-cpt-pricing-table' ),
+		'singular_name'       => _x( 'Price table item', 'Post Type Singular Name', 'startup-cpt-pricing-table' ),
+		'menu_name'           => __( 'Pricing table', 'startup-cpt-pricing-table' ),
+		'name_admin_bar'      => __( 'Pricing table', 'startup-cpt-pricing-table' ),
+		'parent_item_colon'   => __( 'Parent Item:', 'startup-cpt-pricing-table' ),
+		'all_items'           => __( 'All Items', 'startup-cpt-pricing-table' ),
+		'add_new_item'        => __( 'Add New Item', 'startup-cpt-pricing-table' ),
+		'add_new'             => __( 'Add New', 'startup-cpt-pricing-table' ),
+		'new_item'            => __( 'New Item', 'startup-cpt-pricing-table' ),
+		'edit_item'           => __( 'Edit Item', 'startup-cpt-pricing-table' ),
+		'update_item'         => __( 'Update Item', 'startup-cpt-pricing-table' ),
+		'view_item'           => __( 'View Item', 'startup-cpt-pricing-table' ),
+		'search_items'        => __( 'Search Item', 'startup-cpt-pricing-table' ),
+		'not_found'           => __( 'Not found', 'startup-cpt-pricing-table' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'startup-cpt-pricing-table' )
 	);
 	$args = array(
-		'label'               => __( 'pricing', 'text_domain' ),
+		'label'               => __( 'pricing', 'startup-cpt-pricing-table' ),
 		'labels'              => $labels,
 		'supports'            => array( 'title', 'revisions' ),
 		'hierarchical'        => true,
@@ -112,50 +112,50 @@ function startup_reloaded_pricing_meta() {
 
 	$cmb_box = new_cmb2_box( array(
 		'id'            => $prefix . 'metabox',
-		'title'         => __( 'Pricing details', 'cmb2' ),
+		'title'         => __( 'Pricing details', 'startup-cpt-pricing-table' ),
 		'object_types'  => array( 'pricing' )
 	) );
     
     $cmb_box->add_field( array(
-		'name'       => __( 'Currency', 'cmb2' ),
+		'name'       => __( 'Currency', 'startup-cpt-pricing-table' ),
 		'id'         => $prefix . 'currency',
         'default'     => '$',
 		'type'       => 'text'
 	) );
 
     $cmb_box->add_field( array(
-		'name'       => __( 'Price', 'cmb2' ),
+		'name'       => __( 'Price', 'startup-cpt-pricing-table' ),
 		'id'         => $prefix . 'price',
 		'type'       => 'text'
 	) );
     
     $cmb_box->add_field( array(
-		'name'       => __( 'Unit', 'cmb2' ),
+		'name'       => __( 'Unit', 'startup-cpt-pricing-table' ),
 		'id'         => $prefix . 'unit',
         'default'     => '/MO.',
 		'type'       => 'text'
 	) );
     
     $cmb_box->add_field( array(
-		'name'             => __( 'Featured', 'cmb2' ),
+		'name'             => __( 'Featured', 'startup-cpt-pricing-table' ),
 		'id'               => $prefix . 'featured',
 		'type'             => 'checkbox'
 	) );
     
     $cmb_box->add_field( array(
-		'name'       => __( 'Button text', 'cmb2' ),
+		'name'       => __( 'Button text', 'startup-cpt-pricing-table' ),
 		'id'         => $prefix . 'button_text',
 		'type'       => 'text'
 	) );
     
     $cmb_box->add_field( array(
-		'name'       => __( 'Button url', 'cmb2' ),
+		'name'       => __( 'Button url', 'startup-cpt-pricing-table' ),
 		'id'         => $prefix . 'button_url',
 		'type'       => 'text'
 	) );
     
     $cmb_box->add_field( array(
-        'name'             => __( 'Icon 1', 'cmb2' ),
+        'name'             => __( 'Icon 1', 'startup-cpt-pricing-table' ),
         'id'               => $prefix . 'icon_1',
         'type'             => 'select',
         'show_option_none' => true,
@@ -163,13 +163,13 @@ function startup_reloaded_pricing_meta() {
     ) );
 
     $cmb_box->add_field( array(
-		'name'       => __( 'Text 1', 'cmb2' ),
+		'name'       => __( 'Text 1', 'startup-cpt-pricing-table' ),
 		'id'         => $prefix . 'text_1',
 		'type'       => 'text'
 	) );
     
     $cmb_box->add_field( array(
-        'name'             => __( 'Icon 2', 'cmb2' ),
+        'name'             => __( 'Icon 2', 'startup-cpt-pricing-table' ),
         'id'               => $prefix . 'icon_2',
         'type'             => 'select',
         'show_option_none' => true,
@@ -177,13 +177,13 @@ function startup_reloaded_pricing_meta() {
     ) );
 
     $cmb_box->add_field( array(
-		'name'       => __( 'Text 2', 'cmb2' ),
+		'name'       => __( 'Text 2', 'startup-cpt-pricing-table' ),
 		'id'         => $prefix . 'text_2',
 		'type'       => 'text'
 	) );
     
     $cmb_box->add_field( array(
-        'name'             => __( 'Icon 3', 'cmb2' ),
+        'name'             => __( 'Icon 3', 'startup-cpt-pricing-table' ),
         'id'               => $prefix . 'icon_3',
         'type'             => 'select',
         'show_option_none' => true,
@@ -191,13 +191,13 @@ function startup_reloaded_pricing_meta() {
     ) );
 
     $cmb_box->add_field( array(
-		'name'       => __( 'Text 3', 'cmb2' ),
+		'name'       => __( 'Text 3', 'startup-cpt-pricing-table' ),
 		'id'         => $prefix . 'text_3',
 		'type'       => 'text'
 	) );
     
     $cmb_box->add_field( array(
-        'name'             => __( 'Icon 4', 'cmb2' ),
+        'name'             => __( 'Icon 4', 'startup-cpt-pricing-table' ),
         'id'               => $prefix . 'icon_4',
         'type'             => 'select',
         'show_option_none' => true,
@@ -205,7 +205,7 @@ function startup_reloaded_pricing_meta() {
     ) );
 
     $cmb_box->add_field( array(
-		'name'       => __( 'Text 4', 'cmb2' ),
+		'name'       => __( 'Text 4', 'startup-cpt-pricing-table' ),
 		'id'         => $prefix . 'text_4',
 		'type'       => 'text'
 	) );
