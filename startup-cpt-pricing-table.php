43dd312bc3dcfae7fdb53a036bd131e2844e826e
yozz <yozz.online@gmail.com>
2016-05-04 10:35:46 -0400

    Dependencies

--- startup-cpt-pricing-table.php
+++ startup-cpt-pricing-table.php
@@ -16,6 +16,9 @@ if ( ! defined( 'WPINC' ) ) {
 //Include this to check if a plugin is activated with is_plugin_active
 include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
+//Include this to check dependencies
+include_once( 'inc/dependencies.php' );
+
 //GitHub Plugin Updater
 function startup_cpt_pricing_table_updater() {
 	include_once 'lib/updater.php';
@@ -113,25 +116,6 @@ function startup_cpt_pricing_table_caps() {
 register_activation_hook( __FILE__, 'startup_cpt_pricing_table_caps' );
 
 // Metaboxes
-/**
- * Detection de CMB2. Identique dans tous les plugins.
- */
-if ( !function_exists( 'cmb2_detection' ) ) {
-    function cmb2_detection() {
-        if ( !defined( 'CMB2_LOADED' ) ) {
-            add_action( 'admin_notices', 'cmb2_notice' );
-        }
-    }
-
-    function cmb2_notice() {
-        if ( current_user_can( 'activate_plugins' ) ) {
-            echo '<div class="error message"><p>' . __( 'CMB2 plugin or StartUp Reloaded theme must be active to use custom metaboxes.', 'startup-cpt-pricing-table' ) . '</p></div>';
-        }
-    }
-
-    add_action( 'init', 'cmb2_detection' );
-}
-
 function startup_cpt_pricing_table_meta() {
     require ABSPATH . 'wp-content/plugins/startup-cpt-pricing-table/inc/font-awesome.php';
 
@@ -261,25 +245,6 @@ function startup_cpt_pricing_table_shortcode( $atts ) {
 add_shortcode( 'pricing-table', 'startup_cpt_pricing_table_shortcode' );
 
 // Shortcode UI
-/**
- * Detection de Shortcake. Identique dans tous les plugins.
- */
-if ( !function_exists( 'shortcode_ui_detection' ) ) {
-    function shortcode_ui_detection() {
-        if ( !function_exists( 'shortcode_ui_register_for_shortcode' ) ) {
-            add_action( 'admin_notices', 'shortcode_ui_notice' );
-        }
-    }
-
-    function shortcode_ui_notice() {
-        if ( current_user_can( 'activate_plugins' ) ) {
-            echo '<div class="error message"><p>' . __( 'Shortcake plugin must be active to use fast shortcodes.', 'startup-cpt-pricing-table' ) . '</p></div>';
-        }
-    }
-
-    add_action( 'init', 'shortcode_ui_detection' );
-}
-
 function startup_cpt_pricing_table_shortcode_ui() {
 
     shortcode_ui_register_for_shortcode(
